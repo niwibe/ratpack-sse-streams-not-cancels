75430ed71eb7952547b8f5cdb26ce58033d73f56
Andrey Antukh <niwi@niwi.nz>
2015-05-26 13:04:00 +0200

    Add code for reproduce the unexpected exception when using async publishers.

--- src/main/java/my/app/Main.java
+++ src/main/java/my/app/Main.java
@@ -2,80 +2,40 @@ package my.app;
 
 import ratpack.server.RatpackServer;
 import ratpack.handling.*;
-import ratpack.sse.*;
+import ratpack.stream.*;
+import ratpack.http.*;
 import ratpack.func.*;
+
 import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-import java.time.Instant;
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
 
-import static ratpack.sse.ServerSentEvents.serverSentEvents;
-
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 
 public class Main {
-  public static class MyPublisher<T> implements Publisher<T> {
-    public MyPublisher() {
-    }
-
-    @Override
-    public void subscribe(Subscriber<? super T> s) {
-      System.out.println("subscribe");
-      Subscription subscription = new Subscription() {
-          @Override
-          public void request(long n) {
-            System.out.println("request" + n);
-
-            Thread t = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                  try {
-                    Thread.sleep(1000);
-                    for (long x=0; x<n; x++) {
-                      Instant time = Instant.now();
-                      s.onNext((T)time.toString());
-                    }
-                  } catch (InterruptedException e) {
-                  }
-                }
-              });
-
-            t.start();
-          }
-
-          @Override
-          public void cancel() {
-            // This is never executed when connection is closed prematirelly
-            System.out.println("cancel");
-          }
-        };
-
-      s.onSubscribe(subscription);
-    }
-  }
-
   public static class MyHandler implements Handler {
     public void handle(Context context) {
-
-      Publisher<String> p = new MyPublisher();
-
-      ServerSentEvents events = serverSentEvents(p, new Action<Event>() {
-          public void execute(Event e) throws Exception {
-            e.id(Objects::toString).event("counter").data(i -> "event " + i);
+      List<String> messages = Arrays.asList("Hello", "World!", "How", "Are", "You");
+      Publisher<String> p1 = new AsyncIterablePublisher<String>(messages);
+      Publisher<ByteBuf> p2 = Streams.map(p1, new Function<String,ByteBuf>() {
+          public ByteBuf apply(final String input) {
+            try {
+              return Unpooled.wrappedBuffer(input.getBytes("UTF-8"));
+            } catch (java.io.UnsupportedEncodingException e) {
+              return Unpooled.EMPTY_BUFFER;
+            }
           }
         });
 
-      context.render(events);
+      Response response = context.getResponse();
+      response.sendStream(p2);
     }
   }
 
-
   public static void main(String... args) throws Exception {
     RatpackServer.start(server -> server
-      .handlers(chain -> chain
-        .get(ctx -> ctx.render("Hello World!"))
-        .get("events", new MyHandler())
-      )
+      .handlers(chain -> chain.get(new MyHandler()))
     );
   }
 }
