98232ae92d5d4d363225439bea7beb8ba533db5a
Andrey Antukh <niwi@niwi.nz>
2016-03-21 12:48:53 +0200

    Add test app.

--- src/main/java/my/app/Main.java
+++ src/main/java/my/app/Main.java
@@ -1,80 +1,44 @@
 package my.app;
 
 import ratpack.server.RatpackServer;
-import ratpack.handling.*;
-import ratpack.sse.*;
-import ratpack.func.*;
+import ratpack.handling.Context;
+import ratpack.handling.Handler;
+import ratpack.stream.Streams;
+
 import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-import java.time.Instant;
-import java.util.Objects;
+import org.reactivestreams.Publisher;
 
-import static ratpack.sse.ServerSentEvents.serverSentEvents;
+import java.time.Duration;
+import java.nio.CharBuffer;
 
+import io.netty.buffer.Unpooled;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.util.CharsetUtil;
 
 public class Main {
-  public static class MyPublisher<T> implements Publisher<T> {
-    public MyPublisher() {
-    }
-
-    @Override
-    public void subscribe(Subscriber<? super T> s) {
-      System.out.println("subscribe");
-      Subscription subscription = new Subscription() {
-          @Override
-          public void request(long n) {
-            System.out.println("request" + n);
-
-            Thread t = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                  try {
-                    Thread.sleep(1000);
-                    for (long x=0; x<n; x++) {
-                      Instant time = Instant.now();
-                      s.onNext((T)time.toString());
-                    }
-                  } catch (InterruptedException e) {
-                  }
-                }
-              });
-
-            t.start();
-          }
-
-          @Override
-          public void cancel() {
-            // This is never executed when connection is closed prematirelly
-            System.out.println("cancel");
-          }
-        };
-
-      s.onSubscribe(subscription);
-    }
-  }
-
   public static class MyHandler implements Handler {
-    public void handle(Context context) {
-
-      Publisher<String> p = new MyPublisher();
-
-      ServerSentEvents events = serverSentEvents(p, new Action<Event>() {
-          public void execute(Event e) throws Exception {
-            e.id(Objects::toString).event("counter").data(i -> "event " + i);
+    public void handle(Context ctx) {
+      Publisher<ByteBuf> strings = Streams.periodically(ctx, Duration.ofMillis(5), (i) -> {
+          if (i < 5) {
+            return  ByteBufUtil.encodeString(UnpooledByteBufAllocator.DEFAULT,
+                                             CharBuffer.wrap(i.toString()),
+                                             CharsetUtil.UTF_8);
+          } else {
+            return null;
           }
         });
 
-      context.render(events);
+      ctx.getResponse().sendStream(strings);
     }
   }
-
 
   public static void main(String... args) throws Exception {
     RatpackServer.start(server -> server
       .handlers(chain -> chain
         .get(ctx -> ctx.render("Hello World!"))
-        .get("events", new MyHandler())
+        .get("sample", new MyHandler())
       )
     );
   }
